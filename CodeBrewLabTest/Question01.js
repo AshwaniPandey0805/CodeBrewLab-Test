f316e4b380fc4bd484fa07684332d8ac7b0d8246
Ashwani Kumar Pandey <ashwanipandey679@gmail.com>
2024-01-30 13:44:54 +0530

    Question 01 Completed

---
+++ CodeBrewLabTest/Question01.js
@@ -0,0 +1,16 @@
+const arr = [-2, 5, 3, -8, 10, -4]
+
+function sumOfPositives(numsArray){
+    let sum = 0;
+
+    for(let i = 0; i < numsArray.length; i++){
+        if(numsArray[i] > 0){
+            sum += numsArray[i]
+        }
+    }
+
+    return sum;
+}
+
+const result01 = sumOfPositives(arr);
+console.log(result01);
